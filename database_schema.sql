0fd30430115a5c6c7d1cdfa8af0ec24deb8aa534
keys <keonimar@gmail.com>
2018-07-26 21:32:10 +0900

    add mock on users.py. change user column name on databases.

--- database_schema.sql
+++ database_schema.sql
@@ -3,52 +3,52 @@
 create table tokens (
     rowid integer auto_increment,
     token bigint,
-    user_num integer,
+    user_id integer,
     ctime datetime not null,
     primary key (token),
 )
 
 create table users (
-    user_num integer auto_increment,
-    user_id varchar(64),
+    user_id integer auto_increment,
+    username varchar(64),
     password varchar(64) not null,
     ctime datetime,
     mtime datetime,
-    primary key (user_num),
-    index index_id (user_id),
+    primary key (user_id),
+    index index_name (username),
 )
 
 create table boxes (
-    box_num integer auto_increment,
-    user_num integer,
+    box_id integer auto_increment,
+    user_id integer,
     box_title varchar(32) not null,
     ctime datetime,
     mtime datetime,
-    primary key (box_num),
-    index index_user (user_num),
+    primary key (box_id),
+    index index_user (user_id),
 )
 
 create table cards (
-    card_num integer auto_increment,
-    box_num integer,
-    user_num integer,
+    card_id integer auto_increment,
+    box_id integer,
+    user_id integer,
     card_title varchar(32) not null,
     ctime datetime,
     mtime datetime,
     recent_records char(340),
-    primary key (card_num),
-    index index_user_box (user_num, box_num),
+    primary key (card_id),
+    index index_user_box (user_id, box_id),
 )
 
 create table records (
     rowid integer auto_increment,
-    card_num integer,
+    card_id integer,
     record char(16),
     primary key (rowid),
-    index index_card (card_num),
+    index index_card (card_id),
 )
 
-select * from boxes where user_num=$UID
-select * from cards where user_num=$UID
-select * from cards where user_num=$UID and box_num=$BID
-select record from records where card_num=$CID
+select * from boxes where user_id=$UID
+select * from cards where user_id=$UID
+select * from cards where user_id=$UID and box_id=$BID
+select record from records where card_id=$CID
--- ink/datastore/users.py
+++ ink/datastore/users.py
@@ -1,6 +1,47 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import mysql.connector
+
 
 class Users:
-    pass
+    db_conn_elem: dict
+
+    def __init__(self):
+        self.conn = mysql.connector.connect(self.db_conn_elem)
+
+    def get_uid(self, username: str) -> int:
+        sql_stmt = '''
+            select user_id from users where username = '?'
+        '''
+        cur = self.conn.cursor()
+        sql_args = list(username)
+        cur.execute(sql_stmt, sql_args)
+        row = cur.fetchone()
+        if row:
+            return int(row[0])
+        else:
+            return 0
+
+    def get_username(self, user_id: int) -> str:
+        sql_stmt = '''
+            select username from users where user_id = ?
+        '''
+        cur = self.conn.cursor()
+        sql_args = list(user_id)
+        cur.execute(sql_stmt, sql_args)
+        row = cur.fetchone()
+        if row:
+            return str(row[0])
+        else:
+            return ''
+
+    def add_user(self, username: str, password: str) -> int:
+        # XXX
+        return self.get_uid(username)
+
+    def delete_user(self, user_id: int) -> bool:
+        pass
+
+    def auth_user(self, user_id: int, password: str) -> bool:
+        pass
