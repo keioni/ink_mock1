9328fb4cd214e2dba523d6ea13698b4d16147809
keys <keonimar@gmail.com>
2018-09-05 00:58:51 +0900

    impl. __getattr__ method but it is not good idea so comment out

--- ink/sys/config.py
+++ ink/sys/config.py
@@ -19,6 +19,27 @@ class ConfigManager(configparser.ConfigParser):
             filename = os.path.dirname(__file__) + '/settings.cfg'
         super(ConfigManager, self).read(filename, encoding)
 
+    # def __getattr__(self, name):
+    #     if name == 'db_connect_config':
+    #         db_conf = dict()
+    #         if section_name in self:
+    #             for key in self[section_name]:
+    #                 value = self[section_name][key]
+    #                 if key == 'port':
+    #                     value = int(value)
+    #                 db_conf[key] = value
+    #         return db_conf
+    #     msg = '{} object has no attribute {}'.format(type(self), name)
+    #     raise AttributeError(msg)
+
+    # def __setattr__(self, name, value):
+    #     msg = ''
+    #     if name == 'db_connect_config':
+    #         msg = 'readonly attribute {}'.format(type(self))
+    #     if msg:
+    #         raise AttributeError(msg)
+    #     super().__setattr__(name, value)
+
     def get_db_connect_config(self, section_name: str = 'database'):
         '''
         db_connect_config return a string used for mysql.connect() method.
